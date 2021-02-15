71f3b60fa35a6c3d0fc8cd5ca4a0e6329c504b54
mutantenemy <thaisservin@gmail.com>
2021-02-15 09:34:29 +0000

    Removed shrt2long conversion

--- encoder.py
+++ encoder.py
@@ -25,46 +25,46 @@ class Encoder():
         return newURL
 
 
-    def shrt2long (self, newURL):
-        """ Transforms an encoded URL into the actual destiny"""
-        destiny = None
-        logging.info("Looking for " + newURL + " actual destiny")
+    # def shrt2long (self, newURL):
+    #     """ DEPRECATED Transforms an encoded URL into the actual destiny"""
+    #     destiny = None
+    #     logging.info("Looking for " + newURL + " actual destiny")
 
-        # Does the generated URL already exist?
-        for key in self.url2id.keys():
-            logging.debug("Looking into the dictonary for key: " + key + " which contains " + str(self.url2id[key]))
-            # The generated URL exists in the dictionary
-            if newURL[-1] == str(self.url2id[key]):
-                logging.info("I've found " + str(key) + " for the URL code " + newURL[-1])
-                destiny = str(key)
-                return (newURL + " is actually " + destiny)
-        # The generated URL doesn't exist in the dictionary
-        logging.warn(newURL + " was no were to be found in the dictionary.\nUse DEBUG logs to view the contents of the dictionary.")
-        logging.debug(self.url2id.items())
-        return ("Destiny for " + newURL + " has not being created. Try adding it first.")
-
-
-    def getCode (self, key = None):
-        """ Print in console all the tuples\n
-        use a KEY to print a specific tuple"""
-        if key:
-            try:
-                self.url2id[key]
-            except KeyError:
-                logging.warn ("encoder.getCode() failed to find key " + key + ".\nKeyError has been correctly handled.")
-                return ("Destiny " + key + " has not being created. Try adding it first.")
-            else:
-                return (key + ":" + str(self.url2id[key]))
-            finally:
-                logging.warn ("encoder.getCode() has not returned correctly from it's TRY")
-                # return False
-        else:
-            return self.url2id.items()
+    #     # Does the generated URL already exist?
+    #     for key in self.url2id.keys():
+    #         logging.debug("Looking into the dictonary for key: " + key + " which contains " + str(self.url2id[key]))
+    #         # The generated URL exists in the dictionary
+    #         if newURL[-1] == str(self.url2id[key]):
+    #             logging.info("I've found " + str(key) + " for the URL code " + newURL[-1])
+    #             destiny = str(key)
+    #             return (newURL + " is actually " + destiny)
+    #     # The generated URL doesn't exist in the dictionary
+    #     logging.warn(newURL + " was no were to be found in the dictionary.\nUse DEBUG logs to view the contents of the dictionary.")
+    #     logging.debug(self.url2id.items())
+    #     return ("Destiny for " + newURL + " has not being created. Try adding it first.")
 
 
-    def check4Existing(self, key):
-        """ Check if URL exists in the dictionary """
-        return key in self.url2id
+    # def getCode (self, key = None):
+    #     """ DEPRECATED - Print in console all the tuples\n
+    #     use a KEY to print a specific tuple"""
+    #     if key:
+    #         try:
+    #             self.url2id[key]
+    #         except KeyError:
+    #             logging.warn ("encoder.getCode() failed to find key " + key + ".\nKeyError has been correctly handled.")
+    #             return ("Destiny " + key + " has not being created. Try adding it first.")
+    #         else:
+    #             return (key + ":" + str(self.url2id[key]))
+    #         finally:
+    #             logging.warn ("encoder.getCode() has not returned correctly from it's TRY")
+    #             # return False
+    #     else:
+    #         return self.url2id.items()
+
+
+    # def check4Existing(self, key):
+    #     """ DEPRECATED - Check if URL exists in the dictionary """
+    #     return key in self.url2id
 
     def base62(self, index):
         """ Transform an INT into an encoded string on base62"""
